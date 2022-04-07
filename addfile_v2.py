deedff4083000da28e2be76a39b4976c6626a250
Federico Greco <90803782+federoccco@users.noreply.github.com>
2022-04-07 15:37:12 +0200

    Update addfile_v2.py

--- addfile_v2.py
+++ addfile_v2.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import argparse
 
-files_list = os.listdir("files")  # Creo una lista dei file e cartelle contenuti nella cartella files
+files_list = os.listdir("files")  # Creating a list of the files and folders that are in the "files" folder 
 
 extensions = {
     "Images": [".jpg", ".jpeg", ".png"],
@@ -20,79 +20,51 @@ def files_order(file_to_move):
     string stating the directory from which the file was taken, and the directory in which the file was moved. If the
     argument passed is not a valid name or the file does not exist in the files directory, the function will return a
     string stating that the file does not exist indeed."""
-    cwd = os.getcwd()  # Ottengo il path della current working directory
+    
+    cwd = os.getcwd()  # Getting current working directory path
     if os.path.isfile(f"files/{file_to_move}"):
+        file_name, extension = os.path.splitext(file_to_move)  # Getting the file estension
+        for key in extensions:  # Looping the dictionary, "key" will take the dictionary key associated value at each cycle
+            if extension in extensions[key]:  # Checking that the file extension is in the dictionary
 
-        """
-        ho eliminato il ciclo, al posto di controllare che esiste un file che si chiami come il nome fornito dall' 
-        utente controllo direttamente che quel file esista e se esiste utilizzo le sue info per (eventualmente) creare
-        una cartella e stampare informazioni
-        """
-        file_name, extension = os.path.splitext(file_to_move)  # Isolo l' estensione del file
-        for key in extensions:  # Ciclo nel dizionario, "key" sarà uguale alla chiave del dizionario ad ogni ciclo
-            if extension in extensions[key]:  # Controllo che l' estensione del file sia presente nel mio dizionario
-
-                # Controllo che esista una directory che si chiami come la chiave del dizionario, nel caso non
-                # dovesse esistere, la creo.
+                # Checking that a directory with the same name of the key dictionary, in case it doesn't exist, i create it.
                 if not os.path.isdir(f"files/{key}"):
                     os.makedirs(f"files/{key}")
-                src = f"files/{file_to_move}"  # Creo una variabile con il path di origine
-                dst = f"files/{key}/{file_to_move}"  # E una con il path di destinazione
-                shutil.move(src=src, dst=dst)  # Sposto il file da origine a destinazione
-                file_info = f"{file_name} type: {key} size: {os.path.getsize(dst)}B"  # Le informazioni del file
+                src = f"files/{file_to_move}"  # Declaring a variable with the starting path
+                dst = f"files/{key}/{file_to_move}"  # And one with the destination path
+                shutil.move(src=src, dst=dst)  # Moving the file from starting to destination
+                file_info = f"{file_name} type: {key} size: {os.path.getsize(dst)}B"  # File's infos
                 return f"{file_info} was correctly moved from {cwd}/{src} to {cwd}/{dst}"
     else:
         return f"{file_to_move} does not exist"
 
-    """
-    l' else di prima aspettava di verificare che l' intero ciclo finisse, e nel caso di risposta negativa veniva 
-    eseguito, l' ho inserito come else clause del ciclo for perché se indentavo come else clause dell' if statement
-    di controllo, mi veniva stampato per ogni ciclo del file.  Però come nell' esempio dei commenti, l' else viene 
-    eseguito comunque a fine ciclo a meno che non ci sia una prova di interruzione. La mia idea era quella di 
-    utilizzare return come prova del successo o meno del ciclo (e come prova di interruzione). Nel caso in cui  non 
-    avessi avuto un return, allora sarebbe stato eseguito l' else clause. Forse questo è un caso particolar in cui me 
-    la sono cavata grazie alla presenza di return, che deve essere univoco, ma invece che comunicare subito che il file
-    non esiste, lo script eseguiva prima tutto il ciclo, con un carico inutile, per poi comunicare all' utente che 
-    il file non esiste. Di norma l' else di un for loop viene sempre eseguito ad esaurimento ciclo a meno che non 
-    incontri un break statement, che avrei dovuto implementare.  la clausola diventi falsa (while loop)
-     
-     Ora è semplicemente un else in alternativa al False state dell' if statement di controllo 
-     per l' esistenza del file fornito dall' utente
-    """
 
-
-# Creo un oggetto di tipo ArgumentParser a cui andrò ad associare i vari argomenti ed opzioni che voglio utilizzare
+# Making an ArgumentParser type object which i will associate options and arguments i will use
 parser = argparse.ArgumentParser(
     description="""
 The script will move a file from his original directory to another based on its extension. If the directory doesn't
 exist, the script will automatically create one, named with the extension's type name. E.g. .mp3 -->Audio, .txt -->Docs
 """)
 
-# Tramite parser.add_argument, aggiungo i parametri richiesti ed opzionali che è necessario scrivere nel cmd.
-# in questo caso aggiungerò il nome del file che voglio spostare, nonché parametro della funzione definita ad inizio
-# script. La funzione che ho creato prende un solo argomento, quindi lo aggiungo rendendo obbligatorio il suo
-# inserimento al fine del funzionamento corretto dello script, in caso di mancato inserimento, l' utente verrà avvisato
+# Through parser.add_argument, i am adding the requested and optional parameters that are necessary when typing in cmd.
+# in this case i am adding: the file name i want to move, i.e. parameter of the declared function above
+# The function takes a single argument, by adding it i make its use mandatory for the correct functionality of the script.
+# In case of missing argument, the user will be warned
 parser.add_argument(
-    # L' argomento è richiesto implicitamente in quanto dichiarato come positional argument e non come series of flags
-    # pertanto il parametro "required=True" non è necessario, dato che serve solo nel caso di argomenti opzionali
-    "file_to_move",  # l' argomento ricevuto come input dall' utente che sarà processato poi nella funzione
-    type=str,  # il tipo che assumerà l' argomento
-    # Una breve descrizione di cosa rappresenta l' argomento inserito
+    # The argument is implicitly requestes because it's a positional argument and not a series of flags. As a consequence 
+    # The parameter "required=True" is not necessary as it is only in case of the presence of optional arguments
+    "file_to_move",  # The input argument the user submitted that will be parsed by the function
+    type=str,  # The type that the argument will take
+    # A short description of what the submitted argument represent
     help="This is the name of the file to move, type the name of the file with its extension E.g. 'Hello.txt'"
 )
 
-# Dopo aver definito l' assegnazione degli argomenti, procedo con l' assegnazione vera e propria richiamando
-# parse_args sull' oggetto creato (parser)
+# After defining the arguments assignment, i procede with the actual assignment by calling parse_argse on the object (parser)
 args = parser.parse_args()
 
-# Richiamo poi la funzione definita passando come argomento args.file_to_move, in questo modo verranno applicate le
-# istruzioni definite tramite il modulo argparse sul parametro passato alla funzione, per poter visualizzare il
-# risultato, lo mando in stampa
+# Calling the function passing args.file_to_move as argument, by doing this the instructions provided through the argparse module will be applied
+# To be able to visualize the result, i'll print it
 print(files_order(args.file_to_move))
 
 
-# Penso che questo step avrei potuto risolverlo in diverse maniere, usando solo sys o solo argparse, ma non vedo la
-# necessità di usarli entrambi. L' unico modo in cui avrei potuto sarebbe stato quello di usare sys.argv[1] come
-# variabile, e invece che fare un else statement nella funziona che ritorna una stringa, scrivere un if statement con
-# sys.argv maggiore di, minore di, o uguale a, per poi specificare dei comportamenti in base alle condizioni
-# soddisfatte.
+
